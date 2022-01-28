78e6e18ae631dcdf0fd57e40817e90cf261cf322
AdrianSadowski <sadow.adrian@gmail.com>
2022-01-28 17:21:43 +0100

    Add endpoint tests

--- server.js
+++ server.js
@@ -25,10 +25,15 @@ app.use((req, res) => {
 });
 
 // connects our backend code with the database
-mongoose.connect('mongodb://localhost:27017/companyDB', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+
+const NODE_ENV = process.env.NODE_ENV;
+let dbURI = '';
+
+if(NODE_ENV === 'production') dbURI = 'url to remote db';
+else if(NODE_ENV === 'test') dbURI = 'mongodb://localhost:27017/companyDBtest';
+else dbURI = 'mongodb://localhost:27017/companyDB';
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 
 db.once('open', () => {
@@ -37,41 +42,9 @@ db.once('open', () => {
 db.on('error', (err) => console.log('Error' + err));
 
 
-app.listen('8000', () => {
+
+const server = app.listen('8000', () => {
   console.log('Server is running on port: 8000');
 });
 
-// mongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
-//     if (err) {
-//       console.log('database Error', err);
-//     } else {
-//       console.log('Successfully connected to the database');
-
-//       const db = client.db('companyDB');
-//       const app = express();
-
-//       app.use(cors());
-//       app.use(express.json());
-//       app.use(express.urlencoded({ extended: false }));
-
-//       app.use((req, res, next) => {
-//         req.db = db;
-//         next();
-//       });
-
-//       app.use('/api', employeesRoutes);
-//       app.use('/api', departmentsRoutes);
-//       app.use('/api', productsRoutes);
-
-//       app.use((req, res) => {
-//         res.status(404).send({ message: 'Not found...' });
-//       });
-
-// app.listen('8000', () => {
-//   console.log('Server is running on port: 8000');
-// });
-
-//     }
-
-//   }
-// );
+module.exports = server;
